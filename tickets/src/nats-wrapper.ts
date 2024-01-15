d3e3bc4d21f7c7d20e78c7430ae9b4e8fea7fe57
Tanay Shah <ts765147@gmail.com>
2024-01-15 16:58:01 +0530

    continue work on tickets directory

---
+++ tickets/src/nats-wrapper.ts
@@ -0,0 +1,20 @@
+import nats, { Stan } from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan; // this property might be undefined for some period of time
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+    return new Promise<void>((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this._client!.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper(); // share this instance to all of the different files
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@chikkipikki2/common";
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 router.post(
@@ -19,7 +21,12 @@ router.post(
     const { title, price } = req.body;
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
-
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.status(201).send(ticket);
   }
 );
